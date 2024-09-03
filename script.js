5caf7e2c7b088945c1347e78baecfb7affcad0f0
ackindev <ackinlino123@gmail.com>
2024-09-03 10:01:26 -0300

    api whatsapp

--- script.js
+++ script.js
@@ -139,6 +139,68 @@ function removeItemCart(name){
         updateCartModal();
 
     }
+    
+}
+
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+// Finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+    
+    // const isOpen = checkRestaurantOpen();
+    // if(!isOpen){
+    //     alert("O restaurante está fechado. Por favor, tente novamente mais tarde.")
+    //     return;
+    // }
+    
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+    //Enviar o pedido para api whats
+    const cartItems = cart.map((item) => {
+        return (
+            `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "67999123445"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+
+})
+
+// Verificar a hora e manipular o card horario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+    //true = restaurante está aberta
 
 }
 
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
